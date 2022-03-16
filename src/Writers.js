e5918597e862492f19b4efcc7736532d9e997033
hickma <hickmat108@gmail.com>
2022-03-16 08:37:11 -0700

    first commit

---
+++ src/Writers.js
@@ -0,0 +1,39 @@
+const Writers=[
+{
+    id:1,
+    Name: "Jamila Lorde",
+    avatar:"audre_Lorde",
+    email: "jamila23@gmail.com",
+    phone: "0248779936"
+},
+    {
+        id:2,
+        Name: "Ibrahim Hawa",
+        avatar:"belle_hooks",
+        email: "jamila23@gmail.com",
+        phone: "0248779936"
+    },
+        {
+            id:3,
+            Name: "Jamila Lorde",
+            avatar:"du_bois",
+            email: "jamila23@gmail.com",
+            phone: "0248779936"
+        },
+            {
+                id:4,
+                Name: "Jamila Lorde",
+                avatar:"james_baldwin",
+                email: "jamila23@gmail.com",
+                phone: "0248779936"
+            },
+                {
+                    id:5,
+                    Name: "Jamila Lorde",
+                    avatar:"maya_angelou",
+                    email: "jamila23@gmail.com",
+                    phone: "0248779936"
+
+}]
+
+export default Writers;
